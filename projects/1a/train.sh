b192dc478e8e46042c75c578fd8de2da597dd1c6
KiruhaLapin <kiruhalapin2012@gmail.com>
2025-03-03 14:38:23 +0000

    asd

--- projects/1a/train.sh
+++ projects/1a/train.sh
@@ -1,18 +1,3 @@
-# Проверяем, что переданы два аргумента
-if [ "$#" -ne 2 ]; then
-    echo "Использование: $0 <номер_проекта> <путь_к_файлу_с_данными>"
-    exit 1
-fi
+#!/bin/bash
 
-# Устанавливаем переменные
-PROJECT_ID=$1
-DATASET_PATH=$2
-
-# Определяем путь к корню репозитория
-REPO_ROOT=$(cd "$(dirname "$0")/../.." && pwd)
-
-# Путь к скрипту train.py
-TRAIN_SCRIPT="$REPO_ROOT/projects/$PROJECT_ID/train.py"
-python3 "$TRAIN_SCRIPT" "$PROJECT_ID" "$DATASET_PATH"
-
-
+projects/$1/train.py $@
