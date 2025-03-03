80e0eee9c3b0be666277bcfa8dd55c1efe9a7c29
KiruhaLapin <kiruhalapin2012@gmail.com>
2025-03-03 13:02:51 +0000

    asd

--- projects/1a/train.sh
+++ projects/1a/train.sh
@@ -13,13 +13,6 @@ REPO_ROOT=$(cd "$(dirname "$0")/../.." && pwd)
 
 # Путь к скрипту train.py
 TRAIN_SCRIPT="$REPO_ROOT/projects/$PROJECT_ID/train.py"
-
-if [[ "$DATASET_PATH" = /* ]]; then
-    FINAL_PATH="$DATASET_PATH"
-else
-    FINAL_PATH="$REPO_ROOT/$DATASET_PATH"
-fi
-
-python3 "$TRAIN_SCRIPT" "$PROJECT_ID" "$FINAL_PATH"
+python3 "$TRAIN_SCRIPT" "$PROJECT_ID" "$DATASET_PATH"
 
 
