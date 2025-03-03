23b1aec5c0106d08e74229dda6388b0b15b7c50f
KiruhaLapin <kiruhalapin2012@gmail.com>
2025-03-03 19:14:57 +0000

    asd

--- projects/1a/filter_cond.py
+++ projects/1a/filter_cond.py
@@ -5,8 +5,11 @@ def filter_cond(line_dict):
     Takes a dict with field names as argument
     Returns True if conditions are satisfied
     """
-    cond_match = (
-       int(line_dict["if1"]) > 20 and int(line_dict["if1"]) < 40
-    ) 
-    return True if cond_match else False
+    try:
+    	cond_match = (
+       		int(line_dict["if1"]) > 20 and int(line_dict["if1"]) < 40
+    	) 
+    	return True if cond_match else False
+    except:
+        return False
 
