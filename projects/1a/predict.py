bbd65c5ff5feb6a9282070e7466782b4bedca61d
KiruhaLapin <kiruhalapin2012@gmail.com>
2025-03-03 20:07:47 +0000

    asd

--- projects/1a/predict.py
+++ projects/1a/predict.py
@@ -28,7 +28,7 @@ read_opts=dict(
 
 
 for df in pd.read_csv(sys.stdin, **read_opts):
-    pred = model.predict_proba(df)
+    pred = model.predict(df)
     out = zip(df.id.values, pred)
-    print("\n".join(["{0},{1}".format(*i) for i in out]))
+    print("\n".join([f"{i[0]},{i[1]}" for i in out]))
 
