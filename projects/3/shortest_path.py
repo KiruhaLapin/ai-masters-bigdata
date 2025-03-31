79049c76b6db280278b87a01f92b94c56ed99323
KiruhaLapin <kiruhalapin2012@gmail.com>
2025-03-31 16:47:58 +0000

    update

--- projects/3/shortest_path.py
+++ projects/3/shortest_path.py
@@ -16,50 +16,41 @@ finish = sys.argv[2]
 path_to_df = sys.argv[3]
 path_to_ans = sys.argv[4]
 
-def find_shortest_path(spark, edges_df, start_user, target_user, max_iter=1000000):
-    """
-    Находит кратчайший путь от start_user до target_user с ранней остановкой
-    :param spark: SparkSession
-    :param edges_df: DataFrame с колонками [user_id, follower_id]
-    :param start_user: ID начального пользователя
-    :param target_user: ID целевого пользователя
-    :param max_iter: максимальное число итераций
-    :return: DataFrame с одной строкой [user_id, distance, path] или пустой, если путь не найден
-    """
+def find_shortest_path(spark, edges_df, start_user, target_user, max_iter=100):
     followers_graph = edges_df.groupBy("user_id")\
                              .agg(F.collect_list("follower_id").alias("followers"))
-    
     distances = spark.createDataFrame(
-        [(start_user, 0, str(start_user))],
-        ["user_id", "distance", "path"]
+        [(start_user, 0, [str(start_user)])],
+        ["user_id", "distance", "paths"]
     )
-    for iteration in range(max_iter):
+
+    for _ in range(max_iter):
         target_reached = distances.filter(F.col("user_id") == target_user)
         if target_reached.count() > 0:
-            return target_reached
-        
+            return target_reached.select("user_id", "distance", F.explode("paths").alias("path"))
+
         temp = distances.join(followers_graph, "user_id", "inner")\
             .select(
                 F.explode("followers").alias("follower"),
                 (F.col("distance") + 1).alias("distance"),
-                F.col("path")
+                F.col("paths")
             )
         
         new_distances = temp.withColumn(
-            "path",
-            F.concat(F.col("path"), F.lit("->"), F.col("follower").cast("string"))
+            "paths",
+            F.expr("transform(paths, x -> concat(x, '->', follower))")
         ).withColumnRenamed("follower", "user_id")
+
         
         distances = distances.union(new_distances)\
             .groupBy("user_id")\
             .agg(
                 F.min("distance").alias("distance"),
-                F.first("path").alias("path")
-            )
- 
-        
-    
-    return distances.filter(F.col("user_id") == target_user)
+                F.collect_list("paths").alias("paths")
+            ).withColumn("paths", F.flatten("paths"))  
+
+    return distances.filter(F.col("user_id") == target_user).select("user_id", "distance",F.explode("paths").alias("path"))
+
 
 
 log_schema = StructType(fields=[
