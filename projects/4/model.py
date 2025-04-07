868e8206ced7a953ac3b30f19b1c6e3356ef5ce7
KiruhaLapin <kiruhalapin2012@gmail.com>
2025-04-07 11:10:23 +0000

    update model

--- projects/4/model.py
+++ projects/4/model.py
@@ -3,12 +3,12 @@ from pyspark.ml.feature import (
     Tokenizer, StopWordsRemover, HashingTF, IDF,
     StringIndexer, VectorAssembler
 )
-from pyspark.ml.regression import GBTRegressor
+from pyspark.ml.regression import LinearRegression
 from pyspark.sql.functions import concat_ws, coalesce, lit
 
 tokenizer_review = Tokenizer(inputCol="reviewText", outputCol="review_words")
 remover_review = StopWordsRemover(inputCol="review_words", outputCol="filtered_review_words")
-tf_review = HashingTF(inputCol="filtered_review_words", outputCol="raw_review_features", numFeatures=1000)
+tf_review = HashingTF(inputCol="filtered_review_words", outputCol="raw_review_features", numFeatures=100)
 idf_review = IDF(inputCol="raw_review_features", outputCol="review_features")
 
 
@@ -17,7 +17,11 @@ assembler = VectorAssembler(
     outputCol="features"
 )
 
-regressor = GBTRegressor(labelCol="overall", featuresCol="features")
+regressor = LinearRegression(
+    labelCol="overall",
+    featuresCol="features",
+    regParam=0.1,  # Регуляризация для предотвращения переобучения
+)
 
 pipeline = Pipeline(stages=[
     tokenizer_review, remover_review, tf_review, idf_review,
